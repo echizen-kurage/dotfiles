e86997ee69d0879b8057384da5e02b12e39d8468
kurage <nekuchikurage@gmail.com>
2013-06-20 00:35:01 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+ln -s $HOME/dotfiles/_bash_profile $HOME/.bash_profile
+ln -s $HOME/dotfiles/_bashrc $HOME/.bashrc
+ln -s $HOME/dotfiles/_vim $HOME/.vim
+ln -s $HOME/dotfiles/_vimrc $HOME/.vimrc
+ls -s $HOME/dotfiles/_gvimrc $HOME/.gvimrc
+
