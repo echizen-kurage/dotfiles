23a7dfe8fe4bd1eabda1a31681ea611e1fb4eb62
kurage <kurage@kurage.local>
2014-02-20 01:07:42 +0900

    typo

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ ln -s $HOME/dotfiles/_bash_profile $HOME/.bash_profile
 ln -s $HOME/dotfiles/_bashrc $HOME/.bashrc
 ln -s $HOME/dotfiles/_vim $HOME/.vim
 ln -s $HOME/dotfiles/_vimrc $HOME/.vimrc
-ls -s $HOME/dotfiles/_gvimrc $HOME/.gvimrc
+ln -s $HOME/dotfiles/_gvimrc $HOME/.gvimrc
 
